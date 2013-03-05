3d280cb7d093b7bab1d2cf3d6a1e1793ba7bfbf8
David JH Shih <djh.shih@gmail.com>
2013-03-05 13:53:05 -0500

    Added r scripts.

--- res/hopach.r
+++ res/hopach.r
@@ -1,7 +1,8 @@
 library(hopach)
 
 X <- read.table("../data/data-matrix.txt", header=FALSE, sep="\t");
-D <- read.table("../data/distance-matrix.txt", header=FALSE, sep="\t");
+D <- as.matrix( read.table("../data/distance-matrix.txt", header=FALSE, sep="\t") );
+k <- 8;
 
 h <- hopach(X, dmat = D, clusters = "best", K = 15, col = "seq", mss = "med", initord = "co", ord = "neighbor")
 write.table(h$clustering$labels, "hopach.txt", row.names=FALSE, col.names=FALSE, sep="\t")
